8a902fc84c6fcf3fb26263c6c19560adfa501d71
philoniare <philoniare@gmail.com>
2024-03-09 19:40:14 +0800

    fix: react import issue with react-scripts

--- packages/react/vitest.config.js
+++ packages/react/vitest.config.js
@@ -1,11 +1,12 @@
-import { defineConfig } from 'vitest/config';
-import react from '@vitejs/plugin-react';
-import tsconfigPaths from 'vitest-tsconfig-paths';
+import { defineConfig } from "vitest/config";
+import react from "@vitejs/plugin-react";
+import tsconfigPaths from "vitest-tsconfig-paths";
 
 export default defineConfig({
-    plugins: [react(), tsconfigPaths()],
-    test: {
-        environment: 'jsdom',
-        setupFiles: ['./src/setupTests.ts'],
-    },
+  plugins: [react(), tsconfigPaths()],
+  test: {
+    environment: "jsdom",
+    setupFiles: ["./src/setupTests.ts"],
+    globals: true,
+  },
 });
